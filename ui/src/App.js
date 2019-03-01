eb1851e77871187d24d82821392f56205e1f70bc
Kaustubh <kkaustubh@us.ibm.com>
2019-03-01 14:53:45 -0800

    Added route for dashboard and add project. Separated CreateProjectButton component.

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,17 +1,22 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import './App.css';
 import Dashboard from './features/dashboard/Dashboard';
 import Header from './common/components/layout/header/Header';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import AddProject from './features/project/AddProject';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Header/>
-        <Dashboard />
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Header/>
+          <Route exact path="/dashboard" component={Dashboard}/>
+          <Route exact path="/addProject" component={AddProject}/>
+        </div>
+      </BrowserRouter>
     );
   }
 }
---
+++ ui/src/features/project/AddProject.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class AddProject extends Component {
+  render() {
+    return (
+      <div>Add project goes here...</div>
+    )
+  }
+}
+
+export default AddProject;
