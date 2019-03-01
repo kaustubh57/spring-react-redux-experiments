401746b1d6363f0080ab7a2e9a529c139a6b1e93
Kaustubh <kkaustubh@us.ibm.com>
2019-03-01 09:06:29 -0800

    Added domain, repository, service, controller layer for Project.

---
+++ microservice/src/main/java/com/experiments/demo/services/ProjectService.java
@@ -0,0 +1,17 @@
+package com.experiments.demo.services;
+
+import com.experiments.demo.domain.Project;
+import com.experiments.demo.repositories.ProjectRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProjectService {
+
+    @Autowired
+    private ProjectRepository projectRepository;
+
+    public Project saveOrUpdateProject(Project project) {
+        return projectRepository.save(project);
+    }
+}
